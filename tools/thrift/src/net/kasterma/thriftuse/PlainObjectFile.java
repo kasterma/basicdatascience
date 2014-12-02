f4e7d0c63320c1e207d96a0698abc976d01fe0ba
Bart Kastermans <kasterma@kasterma.net>
2014-12-02 20:20:22 +0100

    use ObjectInputStream and ObjectOutputStream

---
+++ tools/thrift/src/net/kasterma/thriftuse/PlainObjectFile.java
@@ -0,0 +1,47 @@
+package net.kasterma.thriftuse;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import net.kasterma.thriftex1.LabeledNumber;
+
+public class PlainObjectFile {
+
+	public static void main(String[] args) {
+		LabeledNumber ln = new LabeledNumber("lab", (short) 4);
+		
+		try {
+			FileOutputStream fos;
+			fos = new FileOutputStream("t.tmp");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(ln);
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+			FileInputStream fis;
+			try {
+				fis = new FileInputStream("t.tmp");
+			ObjectInputStream ois;
+				ois = new ObjectInputStream(fis);
+				System.out.println(ois.readObject().toString());
+				ois.close();
+			} catch (ClassNotFoundException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
+	}
+
+}
