831b9f86bc5e232b38fe361abba6a101acb23d43
Bart Kastermans <kasterma@kasterma.net>
2015-01-02 14:43:57 +0100

    update loss

--- drivendata/blood/R/loss.R
+++ drivendata/blood/R/loss.R
@@ -1,6 +1,6 @@
 loss <- function(predictions) {
     predictions$rowidx <- sapply(predictions$X,
                                  f(id, which(train$X==id)))
-    y <- train[predictions$rowidx,]$Made.Donation.in.March.2007
+    y <- as.numeric(as.character(train[predictions$rowidx,]$Made.Donation.in.March.2007))
     -1/nrow(predictions)*sum(y*log(predictions$p) + (1-y)*log(1-predictions$p))
 }
