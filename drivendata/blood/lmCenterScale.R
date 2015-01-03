b44a0d0ae4a4dda2fff5edaf89421d5d6f64b3ef
Bart Kastermans <kasterma@kasterma.net>
2015-01-03 15:45:41 +0100

    WIP understanding outliers

--- drivendata/blood/lmCenterScale.R
+++ drivendata/blood/lmCenterScale.R
@@ -19,8 +19,7 @@ train <- tbl_df(read.csv("data/train.csv"))
 
 test <- tbl_df(read.csv("data/test.csv",
                         colClasses=c("integer",
-                            "numeric", "numeric", "numeric", "numeric",
-                            "numeric")))
+                            "numeric", "numeric", "numeric", "numeric")))
 train <- tbl_df(read.csv("data/train.csv",
                          colClasses=c("integer",
                              "numeric", "numeric", "numeric", "numeric",
@@ -43,58 +42,78 @@ str(train)
 run.on.fold <- function(fold) {
     train.fold <- train[fold,]
     test.fold <- train[-fold,]
-    fit <- lm(Made.Donation.in.March.2007 ~ ., train.fold)
+    fit <- lm(Made.Donation.in.March.2007 ~  Months.since.Last.Donation + Months.since.First.Donation + Number.of.Donations, train.fold)
     pred <- predict(fit, test.fold)
     predlm <- data.frame(X=test.fold$X,
                          p=ifelse(pred < 1, ifelse(pred > 0, pred, 0.0001), 0.9999))
     loss(predlm)
+    list(loss=loss(predlm),coeff=fit$coefficients)
 }
 
-(cv.res <- sapply(seq_along(train.cv.dat), f(idx, run.on.fold(train.cv.dat[[idx]]))))
-mean(cv.res)
+cv.res <- lapply(seq_along(train.cv.dat), f(idx, run.on.fold(train.cv.dat[[idx]])))
+cv.losses <- sapply(cv.res, f(x, x$loss))
+mean(cv.losses)
 
-preProcValues <- preProcess(as.data.frame(train[,2:4]), method=c("BoxCox"))
+preProcValues <- preProcess(as.data.frame(train[,2:4]), method=c("scale", "center", "BoxCox"))
 
 train.pp <- predict(preProcValues, as.data.frame(train[,2:4]))
 
-train <- data.frame(X=train$X, train.pp, Made.Donation.in.March.2007=train$Made.Donation.in.March.2007)
+train.bc <- data.frame(X=train$X, train.pp, Made.Donation.in.March.2007=train$Made.Donation.in.March.2007)
 
 run.on.fold <- function(fold) {
-    train.fold <- train[fold,]
-    test.fold <- train[-fold,]
-    fit <- lm(Made.Donation.in.March.2007 ~ ., train.fold)
+    train.fold <- train.bc[fold,]
+    test.fold <- train.bc[-fold,]
+    fit <- lm(Made.Donation.in.March.2007 ~ Months.since.Last.Donation + Months.since.First.Donation + Number.of.Donations, train.fold)
+#    print(fit$coefficients)
     pred <- predict(fit, test.fold)
     predlm <- data.frame(X=test.fold$X,
                          p=ifelse(pred < 1, ifelse(pred > 0, pred, 0.0001), 0.9999))
-    loss(predlm)
+    list(loss=loss(predlm),coeff=fit$coefficients)
 }
 
 
-(cv.res.bc <- sapply(seq_along(train.cv.dat), f(idx, run.on.fold(train.cv.dat[[idx]]))))
-mean(cv.res.bc)
+cv.res.bc <- lapply(seq_along(train.cv.dat), f(idx, run.on.fold(train.cv.dat[[idx]])))
+cv.bc.losses <- sapply(cv.res.bc, f(x, x$loss))
+mean(cv.bc.losses)
 
 ## Compare the results
-boxplot(res ~ id, data=rbind(data.frame(id=1,res=cv.res),
-                      data.frame(id=2,res=cv.res.bc)))
+boxplot(res ~ id, data=rbind(data.frame(id=1,res=cv.losses),
+                      data.frame(id=2,res=cv.bc.losses)))
 
-xx <- melt(data.frame(id=seq_along(cv.res),cv.res, cv.res.bc), id.vars="id")
+xx <- melt(data.frame(id=seq_along(cv.res),cv.losses, cv.bc.losses), id.vars="id")
 ggplot(xx, aes(x=id, y=value, color=variable)) + geom_point()
 
 boxplot(res ~ id, data=rbind(data.frame(id="plain",res=cv.res[c(-3)]),
                       data.frame(id="bc",res=cv.res.bc[c(-3)])))
 
 
-test.predict <- function(pred.fn) {
-    pred <- predict(pred.fn, test)
-    setNames(data.frame(test$X,
-                        ifelse(pred < 1, ifelse(pred > 0, pred, 0.0001), 0.9999)),
-             c("","Made Donation in March 2007"))
-}
 
-lmwithcv <- test.predict(lm(Made.Donation.in.March.2007 ~ ., train))
 
-write.csv(lmwithcv,
-          "lmwithcv.csv",
-          row.names=FALSE)
 
-## results were better than mean and median of the cross validation results
+yy <- data.frame(id=1:10, t(rbind(sapply(cv.res, f(x, x$coeff)), sapply(cv.res.bc, f(x, x$coeff)))))
+names(yy) <- c("id", "a", "b", "c", "d", "a1", "b1", "c1", "d1")
+
+ggplot(yy, aes(x=d, y=d1)) + geom_point() + geom_text(aes(label=id), hjust=2)
+
+GGally::ggpairs(yy[,-1])
+
+zz <- data.frame(id=seq_along(train.cv.dat),
+                 m1=sapply(seq_along(train.cv.dat), f(idx, mean(train$Number.of.Donations[-train.cv.dat[[idx]]]))),
+                 m2=sapply(seq_along(train.cv.dat), f(idx, mean(train.bc$Number.of.Donations[-train.cv.dat[[idx]]]))))
+zz <- melt(zz, id.vars = "id")
+
+ggplot(zz, aes(x=id, y=value, color=variable)) + geom_point()
+
+zz <- data.frame(id=seq_along(train.cv.dat),
+                 m1=sapply(seq_along(train.cv.dat), f(idx, mean(train$Months.since.Last.Donation[-train.cv.dat[[idx]]]))),
+                 m2=sapply(seq_along(train.cv.dat), f(idx, mean(train.bc$Months.since.Last.Donation[-train.cv.dat[[idx]]]))))
+zz <- melt(zz, id.vars = "id")
+
+ggplot(zz, aes(x=id, y=value, color=variable)) + geom_point()
+
+zz <- data.frame(id=seq_along(train.cv.dat),
+                 m1=sapply(seq_along(train.cv.dat), f(idx, mean(train$Months.since.First.Donation[-train.cv.dat[[idx]]]))),
+                 m2=sapply(seq_along(train.cv.dat), f(idx, mean(train.bc$Months.since.First.Donation[-train.cv.dat[[idx]]]))))
+zz <- melt(zz, id.vars = "id")
+
+ggplot(zz, aes(x=id, y=value, color=variable)) + geom_point()
