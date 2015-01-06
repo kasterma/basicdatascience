54f35c173e2315447f8896294e571f22043382f9
Bart Kastermans <kasterma@kasterma.net>
2015-01-06 20:57:22 +0100

    single node working

---
+++ tools/hadoop/vagrant/scripts/basic.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y make emacs23
+## add java
+sudo apt-get install -y openjdk-7-jdk
+## add rsync (mentioned as prereq for hadoop config)
+sudo apt-get install -y rsync
---
+++ tools/hadoop/vagrant/scripts/hadoop.sh
@@ -0,0 +1,3 @@
+wget http://apache.mirrors.pair.com/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
+sudo -u vagrant tar xzf /home/vagrant/hadoop-2.6.0.tar.gz -C /home/vagrant
+cat /vagrant/vagrant/resources/dotprofile >> /home/vagrant/.profile
